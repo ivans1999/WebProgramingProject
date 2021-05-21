9c9d90373fbedf0a31c5f792d204781e7c446015
ivans1999 <stankovicivan1999@gmail.com>
2021-05-21 12:54:02 +0200

    Books controller.

--- ProjekatOWP/src/main/java/com/ftn/ProjekatOWP/controller/BooksController.java
+++ ProjekatOWP/src/main/java/com/ftn/ProjekatOWP/controller/BooksController.java
@@ -1,11 +1,20 @@
 package com.ftn.ProjekatOWP.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Base64;
+import java.util.Date;
 import java.util.InputMismatchException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.annotation.PostConstruct;
@@ -17,16 +26,27 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ftn.ProjekatOWP.model.Book;
+import com.ftn.ProjekatOWP.model.Comment;
+import com.ftn.ProjekatOWP.model.Genre;
+import com.ftn.ProjekatOWP.model.ShoppingCart;
 import com.ftn.ProjekatOWP.model.User;
 import com.ftn.ProjekatOWP.service.BookService;
+import com.ftn.ProjekatOWP.service.CommentService;
+//import com.ftn.ProjekatOWP.service.GenreService;
+import com.ftn.ProjekatOWP.service.ShoppingCartService;
+import com.ftn.ProjekatOWP.service.UserService;
+//import com.mysql.cj.jdbc.Blob;
 
 @Controller
 @RequestMapping(value="/books")
@@ -35,6 +55,15 @@ public class BooksController implements ServletContextAware {
 
 	@Autowired
 	private BookService bookService;
+	
+	@Autowired
+	private CommentService commentService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ShoppingCartService shoppingCartService;
 
 	
 	@Autowired
@@ -51,6 +80,10 @@ public class BooksController implements ServletContextAware {
 		baseURL = servletContext.getContextPath() + "/";
 	}
 	
+	private final String UPLOAD_DIR = "./images/";
+	
+	// ---------------------------- LISTA KNJIGA
+	
 	@GetMapping
 	public ModelAndView index(
 			@RequestParam(required=false) String ISBN,
@@ -59,7 +92,7 @@ public class BooksController implements ServletContextAware {
 			@RequestParam(required=false) String author,
 			@RequestParam(required=false) String yearOfPublication,
 			@RequestParam(required=false) String description,
-			@RequestParam(required=false) byte[] image,
+			@RequestParam(required=false) String image,
 			@RequestParam(required=false) Double price,
 			@RequestParam(required=false) Integer numberOfPages,
 			@RequestParam(required=false) String typeOfCover,
@@ -68,7 +101,7 @@ public class BooksController implements ServletContextAware {
 			HttpSession session, HttpServletResponse response) throws IOException {		
 		
 		
-	
+		
 		
 		List<Book> books = bookService.find(ISBN, name, publishingHouse, author, yearOfPublication, description,
 											image, price, numberOfPages, typeOfCover, letter, numberOfBooks);
@@ -76,16 +109,17 @@ public class BooksController implements ServletContextAware {
 		
 		ModelAndView rezultat = new ModelAndView("books");
 		rezultat.addObject("books", books);
-
 		
+
 		
 		
 		return rezultat;
 	}
 	
 	@GetMapping(value="/Details")
-	public ModelAndView details(@RequestParam Long id, 
+	public ModelAndView details(@RequestParam Long id,
 			HttpSession session, HttpServletResponse response) throws IOException {
+		
 		
 		Book book = bookService.findOne(id);
 		if (book == null) {
@@ -93,39 +127,60 @@ public class BooksController implements ServletContextAware {
 			return null;
 		}
 		
-
+		
+		
+	
 	
 		ModelAndView rezultat = new ModelAndView("oneBook");
 		rezultat.addObject("book", book);
+	
 
 		return rezultat;
 	}
 		
 	
+	
+	// ---------------------------- ADD KNJIGE
+	
 	@GetMapping(value="/Create")
 	public String create(HttpSession session, HttpServletResponse response) throws IOException {
 		
 	
-//		User loggedInUser = (User) session.getAttribute(UserController.USER_KEY);
-//		if (loggedInUser == null || !loggedInUser.isAdministrator()) {
-//			response.sendRedirect(baseURL + "books");
-//			return null;
-//		}
+		User loggedInUser = (User) session.getAttribute(UserController.USER_KEY);
+		if (loggedInUser == null || !loggedInUser.isAdministrator()) {
+			response.sendRedirect(baseURL + "books");
+			return null;
+		}
+
+		
+//		// čitanje
+//		List<Genre> genres = genreService.findAll();
+
+		
+		// prosleđivanje
+		
+//		ModelAndView rezultat = new ModelAndView("addBook");
+//		rezultat.addObject("zanrovi", zanrovi);
 
 		return "addBook";
 	}
 	
 	@PostMapping(value="/Create")
 	public void create(@RequestParam String ISBN, @RequestParam String name, @RequestParam String publishingHouse, @RequestParam String author,
-			@RequestParam String yearOfPublication, @RequestParam String description, @RequestParam byte[] image, @RequestParam Double price, 
+			@RequestParam String yearOfPublication, @RequestParam String description, @RequestParam Double price, 
 			@RequestParam Integer numberOfPages, @RequestParam String typeOfCover, @RequestParam String letter, 
+			
+			@RequestParam("image") MultipartFile multipartFile,
+			
 			HttpSession session, HttpServletResponse response) throws IOException {
 		
 	
+
+		
 		
 		User loggedInUser = (User) session.getAttribute(UserController.USER_KEY);
 		if (loggedInUser == null || !loggedInUser.isAdministrator()) {
-			response.sendRedirect(baseURL + "books/Create");
+			response.sendRedirect(baseURL + "books");
 			return;
 		}
 		    
@@ -172,6 +227,35 @@ public class BooksController implements ServletContextAware {
 		}
 		
 		
+		
+		
+		// -------------------------- UPISIVANJE *-* SLIKE ---
+		
+		// check if file is empty
+        if (multipartFile.isEmpty()) {
+        	throw new IOException("Could not save image file: ");
+           
+        }
+
+        // normalize the file path
+        
+        String image = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        // save the file on the local file system
+        
+        try {
+            Path path = Paths.get(UPLOAD_DIR + image);
+            Files.copy(multipartFile.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+        	
+            e.printStackTrace();
+        }
+
+		
+		
+		
+		
+		
 		Book book = new Book(ISBN, name, publishingHouse, author, yearOfPublication, description, image, price, numberOfPages, typeOfCover, letter);
 		bookService.save(book);
 
@@ -180,9 +264,14 @@ public class BooksController implements ServletContextAware {
 	
 	}
 	
+	
+	
+	
+	// ---------------------------- EDIT KNJIGE
+	
 	@PostMapping(value="/Edit")
 	public void Edit(
-			@RequestParam Long id, @RequestParam byte[] image,
+			@RequestParam Long id, @RequestParam String image,
 			@RequestParam String name, @RequestParam String publishingHouse, @RequestParam String author,
 			@RequestParam Integer numberOfPages, @RequestParam String typeOfCover, @RequestParam String letter,
 			@RequestParam double price, @RequestParam String description, @RequestParam String yearOfPublication,
@@ -203,6 +292,7 @@ public class BooksController implements ServletContextAware {
 			response.sendRedirect(baseURL + "books");
 			return;
 		}
+	
 		
 		
 		
@@ -224,6 +314,90 @@ public class BooksController implements ServletContextAware {
 		response.sendRedirect(baseURL + "books");
 	}
 
+	
+	@GetMapping(value="/AllComments")
+	@ResponseBody
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> allComments(@RequestParam Long bookId, 
+			HttpSession session) throws IOException {
+	
+		
+		List<Comment> comments = commentService.findAll(bookId);
+
+
+		Map<String, Object> odgovor = new LinkedHashMap<>();
+		odgovor.put("status", "ok");
+		odgovor.put("comments", comments);
+		return odgovor;
+	}
+	
+	
+	@PostMapping(value="/AddComment")
+	@ResponseBody
+	public Map<String, Object> create(@RequestParam String text, @RequestParam String ratingComment, @RequestParam Long bookComment, @RequestParam String userComment,  
+			HttpSession session, HttpServletRequest request) throws IOException {
+	
+		User loggedInUser = (User) session.getAttribute(UserController.USER_KEY);
+		if (loggedInUser == null || loggedInUser.isAdministrator()) {
+			
+			Map<String, Object> odgovor = new LinkedHashMap<>();
+			odgovor.put("status", "odbijen");
+			return odgovor;
+		}
+
+		Book bookComment2 = bookService.findOne(bookComment);
+		
+		User userComment2 = userService.findOne(userComment);
+		
+		
+		long yourmilliseconds = System.currentTimeMillis();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");    
+	
+		Date dateComment = new Date(yourmilliseconds);
+		
+		System.out.println(sdf.format(dateComment));
+		
+		
+		try {
+			
+
+			if (text == null || text.equals("")) {
+				throw new Exception("The text entry field must not be blank");
+						
+			}
+			if (ratingComment == null || ratingComment.equals("")) {
+				throw new Exception("The ratingComment entry field must not be blank");
+				
+			}
+
+
+			
+			boolean statusComment=false;
+			
+			Comment comment = new Comment(ratingComment, text, dateComment, statusComment=false, bookComment2, userComment2);
+			commentService.save(comment);
+			
+			Map<String, Object> odgovor = new LinkedHashMap<>();
+			odgovor.put("status", "ok");	
+			return odgovor;
+			
+		} catch (Exception ex) {
+	
+			String poruka = ex.getMessage();
+			if (poruka == "") {
+				poruka = "Failed!";
+			}
+			
+			Map<String, Object> odgovor = new LinkedHashMap<>();
+			odgovor.put("status", "greska");
+			odgovor.put("poruka", poruka);
+			return odgovor;
+		}
+		
+		
+	}
+	
 	
 	
 	
--- ProjekatOWP/src/main/java/com/ftn/ProjekatOWP/service/BookService.java
+++ ProjekatOWP/src/main/java/com/ftn/ProjekatOWP/service/BookService.java
@@ -3,16 +3,19 @@ package com.ftn.ProjekatOWP.service;
 import java.util.List;
 
 import com.ftn.ProjekatOWP.model.Book;
+import com.ftn.ProjekatOWP.model.Genre;
+//import com.mysql.cj.jdbc.Blob;
 
 public interface BookService {
 	
 	Book findOne(Long id);
 	
+//	Book findOne1(String ISBN);
 	
 	List<Book> findAll();
 	
 	List<Book> find(String ISBN, String name, String publishingHouse, String author, String yearOfPublication,
-						String description, byte[] image, Double price, Integer numberOfPages, String typeOfCover, String letter,  Integer numberOfBooks);
+						String description, String image, Double price, Integer numberOfPages, String typeOfCover, String letter,  Integer numberOfBooks);
 
 	
 	
